22d51b36968e672a8a017d0a7788031a7e4cf297
simbo1905 <simbo1905@60hertz.com>
2011-12-14 01:47:55 +0000

    openshift files didn't push from my mac. trying again from linux.

--- src/test/java/org/zkforge/zktodo2/ReminderTest.java
+++ src/test/java/org/zkforge/zktodo2/ReminderTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations={"classpath:dataSourceContext.xml","classpath:default.application-context.xml"})
+@ContextConfiguration(locations={"classpath:dataSourceContext.xml","file:src/main/webapp/WEB-INF/spring/application-context.xml"})
 @Transactional
 public class ReminderTest {
 
